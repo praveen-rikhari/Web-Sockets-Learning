ab14ecb2118a9ec3193c3dfe30e162d893a6dce6
praveen-rikhari <praveenrikhari45@gmail.com>
2024-02-10 21:52:47 +0530

    Storing username and messages

--- Database/chat_Db.sql
+++ Database/chat_Db.sql
@@ -4,6 +4,13 @@ USE chat_db;
 
 CREATE TABLE IF NOT EXISTS chat (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  message TEXT NOT NULL
+  username VARCHAR(255),
+  message TEXT NULL
 );
+
+set SQL_SAFE_UPDATES = 0;
+ALTER TABLE chat MODIFY COLUMN message TEXT NULL;
+
+DROP TABLE chat;
+
+select * from chat;
--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const mysql = require('mysql');
 
 // Create Database connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'todo',
-  database: 'chat_db'
+    host: 'localhost',
+    user: 'root',
+    password: 'todo',
+    database: 'chat_db'
 });
 
 db.connect((err) => {
     if (err) {
-      console.error('Error connecting to MySQL database: ', err);
-      return;
+        console.error('Error connecting to MySQL database: ', err);
+        return;
     }
     console.log('Connected to MySQL database');
-  });
+});
 
 const server = app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -30,24 +30,54 @@ app.use(express.static(__dirname + '/public'));
 // initializing socket.io by passing our server instance
 const io = socketIo(server);
 
+// let activeUsers = [];
+
 io.on('connection', (socket) => {
     console.log("A user connected.");
 
     socket.emit('request-username');
 
+    // Listening for username submission
     socket.on('username', (username) => {
         socket.username = username;
         io.emit('chat-msg', `${username} has joined the chat.`);
+
+        // storing  username in database
+        db.query('INSERT INTO chat (username) VALUES (?)', [username], (err, results) => {
+            if (err) {
+                console.log("User not saved in DATABASE : ", err);
+            } else {
+                console.log('User added to Database.');
+            }
+        })
     });
 
+
+    // Listening for new messages
     socket.on('chat-msg', (msg) => {
         io.emit('chat-msg', `${socket.username}: ${msg}`);
+
+        // storing messages in database
+        db.query('INSERT INTO chat (username, message) VALUES (?, ?)', [socket.username, msg], (err, results) => {
+            if (err) {
+                console.log("Messages not added in DATABASE : ", err);
+            } else {
+                console.log('Messages added to Database.');
+            }
+        })
     });
 
-    socket.on('disconenct', () => {
+    // Listen for disconnection
+    socket.on('disconnect', () => {
         if (socket.username) {
-            io.emit('chat-msg', `${socket.username} has left the chat`)
+            io.emit('chat message', `${socket.username} has left the chat`);
+
+            // Delete user's messages from database
+            db.query('DELETE FROM chat WHERE username = ?', [socket.username], (error, results) => {
+                if (error) throw error;
+            });
         }
-        console.log("A user Disconnect");
-    })
+        console.log('A user disconnected');
+    });
+
 })
